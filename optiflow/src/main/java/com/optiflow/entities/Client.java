6574b3cacc1562cb29abfc6cd6d355c77dff0ec2
VitaoIF <victormelo1212@gmail.com>
2026-05-21 17:57:01 -0300

    Initial commit

---
+++ optiflow/src/main/java/com/optiflow/entities/Client.java
@@ -0,0 +1,41 @@
+package com.optiflow.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "client")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String phone;
+
+    @Column(nullable = false)
+    private String cpf;
+
+    private String address;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDate updatedAt;
+
+}
