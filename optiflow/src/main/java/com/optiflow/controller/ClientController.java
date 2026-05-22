fd5f2ba0bd575c6477dd24d59fc8b0f73f5e58fa
VitaoIF <victormelo1212@gmail.com>
2026-05-22 15:45:01 -0300

    feat: adiciona controller com os metodos crud para client

---
+++ optiflow/src/main/java/com/optiflow/controller/ClientController.java
@@ -0,0 +1,58 @@
+package com.optiflow.controller;
+
+import com.optiflow.dto.request.ClientRequest;
+import com.optiflow.dto.response.ClientResponse;
+import com.optiflow.service.ClientService;
+import org.springframework.data.domain.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/clients")
+public class ClientController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @PostMapping
+    public ResponseEntity<ClientResponse> insert(@RequestBody ClientRequest clientRequest){
+        ClientResponse clientResponse = clientService.insert(clientRequest);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(clientResponse.id())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(clientResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientResponse>> findAll(Pageable pageable){
+        Page<ClientResponse> clientResponsePage = clientService.findAll(pageable);
+        return ResponseEntity.ok().body(clientResponsePage);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientResponse> findById(@PathVariable Long id) {
+        ClientResponse clientResponse = clientService.findById(id);
+        return ResponseEntity.ok().body(clientResponse);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        clientService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientResponse> updated(@PathVariable Long id,@RequestBody ClientRequest request){
+        ClientResponse response = clientService.update(id, request);
+        return ResponseEntity.ok().body(response);
+    }
+}
