dbfd625ca199c006c1a3b99fc9e5fbf13e5cdddf
VitaoIF <victormelo1212@gmail.com>
2026-05-22 16:42:50 -0300

    feat: adiciona entidade Product

---
+++ optiflow/src/main/java/com/optiflow/entities/Product.java
@@ -0,0 +1,44 @@
+package com.optiflow.entities;
+
+import com.optiflow.entities.enums.ProductType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String brand;
+
+    @Enumerated(EnumType.STRING)
+    private ProductType type;
+
+    private Double price;
+
+    private Integer stockQuantity;
+
+    private Boolean active;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDate updatedAt;
+}
