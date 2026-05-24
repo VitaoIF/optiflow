e507a3fdcb9ba00b8c09d240ccfc0db47c7b78c1
VitaoIF <victormelo1212@gmail.com>
2026-05-24 17:51:27 -0300

    feat: adiciona prescriptionController com métodos crud

---
+++ optiflow/src/main/java/com/optiflow/controller/PrescriptionController.java
@@ -0,0 +1,57 @@
+package com.optiflow.controller;
+
+import com.optiflow.dto.request.PrescriptionRequest;
+import com.optiflow.dto.response.PrescriptionResponse;
+import com.optiflow.service.PrescriptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/prescriptions")
+public class PrescriptionController {
+
+    @Autowired
+    private PrescriptionService prescriptionService;
+
+    @PostMapping
+    public ResponseEntity<PrescriptionResponse> insert(@RequestBody PrescriptionRequest request){
+        PrescriptionResponse prescriptionResponse = prescriptionService.insert(request);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(prescriptionResponse.id())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(prescriptionResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<PrescriptionResponse>> findAll(Pageable pageable){
+        Page<PrescriptionResponse> prescriptionResponses = prescriptionService.findAll(pageable);
+        return ResponseEntity.ok().body(prescriptionResponses);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PrescriptionResponse> findById(@PathVariable Long id){
+        PrescriptionResponse prescriptionResponse = prescriptionService.findById(id);
+        return ResponseEntity.ok().body(prescriptionResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(Long id){
+        prescriptionService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PrescriptionResponse> update(@PathVariable Long id, @RequestBody PrescriptionRequest request){
+        PrescriptionResponse prescriptionResponse = prescriptionService.update(id, request);
+        return ResponseEntity.ok().body(prescriptionResponse);
+    }
+}
