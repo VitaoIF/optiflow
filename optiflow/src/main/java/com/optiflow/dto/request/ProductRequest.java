4d185eadf586aefc0363c82f29bb81f9e56ce224
VitaoIF <victormelo1212@gmail.com>
2026-05-27 17:56:15 -0300

    refact: adiciona validação

--- optiflow/src/main/java/com/optiflow/dto/request/ProductRequest.java
+++ optiflow/src/main/java/com/optiflow/dto/request/ProductRequest.java
@@ -1,12 +1,25 @@
 package com.optiflow.dto.request;
 
 import com.optiflow.entities.enums.ProductType;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 
 public record ProductRequest(
+        @NotEmpty(message = "Nome do produto é obrigatório")
         String name,
         String brand,
+
+        @NotNull(message = "Tipo do produto é obrigatório")
         ProductType type,
+
+        @NotNull(message = "Preço é obrigatório")
+        @Positive(message = "Preço deve ser maior que 0")
         Double price,
+
+        @NotNull(message = "Informar a quantiade em estoque é obrigatório")
+        @PositiveOrZero(message = "Quantidade deve ser 0 ou maior")
         Integer stockQuantity,
         Boolean active
 ) {
