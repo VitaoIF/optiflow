f4fb25719f5d64532e7daf0443b1495f73cf2eca
VitaoIF <victormelo1212@gmail.com>
2026-05-27 17:54:10 -0300

    refact: adiciona validação

--- optiflow/src/main/java/com/optiflow/dto/request/ClientRequest.java
+++ optiflow/src/main/java/com/optiflow/dto/request/ClientRequest.java
@@ -1,8 +1,13 @@
 package com.optiflow.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public record ClientRequest(
+        @NotEmpty(message = "Nome é obrigatório")
         String name,
         String phone,
+
+        @NotEmpty(message = "CPF é obrigatório")
         String cpf,
         String address
 ) {
