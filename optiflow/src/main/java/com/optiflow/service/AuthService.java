16e205c7786ca68674a3c02de419acbc6a0370d8
VitaoIF <victormelo1212@gmail.com>
2026-05-31 23:25:40 -0300

    feat: adiciona authService para verificar usuario e senha

---
+++ optiflow/src/main/java/com/optiflow/service/AuthService.java
@@ -0,0 +1,24 @@
+package com.optiflow.service;
+
+import com.optiflow.exceptions.custom.EmailNotFoundException;
+import com.optiflow.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserByEmail(username)
+                .orElseThrow(() -> new EmailNotFoundException("Usuário ou senha inválidos"));
+    }
+}
