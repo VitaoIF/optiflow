8c4dce0c603b6776f98dc110b6eb3cb985db0301
VitaoIF <victormelo1212@gmail.com>
2026-05-31 23:15:56 -0300

    refact: Adiciona método existsByEmail e findUserByEmail que devolve um UserDetails

--- optiflow/src/main/java/com/optiflow/repository/UserRepository.java
+++ optiflow/src/main/java/com/optiflow/repository/UserRepository.java
@@ -2,6 +2,13 @@ package com.optiflow.repository;
 
 import com.optiflow.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<UserDetails> findUserByEmail(String email);
 }
